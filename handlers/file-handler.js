ac9a8c2ac4503b6120ad2fe0342159e155c9dd2a
wangyongqing01 <wangyongqing01@baidu.com>
2016-03-23 18:54:31 +0800

    delete handlers

--- handlers/file-handler.js
+++
@@ -1,5 +0,0 @@
-var $ = require('../lib/utils');
-
-module.exports = function (req, res, next) {
-  var fds =
-}
--- handlers/settings.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (rootFolder, port) {
-  console.log(rootFolder, port);
-  return function (req, res, next) {
-    next();
-  }
-}
