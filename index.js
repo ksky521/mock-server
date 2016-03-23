2b1dd162ce3e0ac40da1f80618d775a723f009a9
wangyongqing01 <wangyongqing01@baidu.com>
2016-03-23 18:28:46 +0800

    package

--- index.js
+++ index.js
@@ -5,55 +5,66 @@ var path = require('path');
 var ipv4 = require('ipv4');
 global.express = require('express');
 var cons = require('consolidate');
-global.app = express();
-var server = require('http').Server(app);
-
-global.io = require('socket.io')(server, {
-  log: false,
-  origins: '*:*' //解决同源策略
-});
-var execDir = process.cwd();
-var rootDir = path.join(__dirname);
-var assetDir = path.join(rootDir, 'assets');
-var projectConfig = {
-  folders: [execDir],
-  curDir: execDir
-};
-var config = {
-  rootDir: rootDir,
-  assetDir: assetDir,
-  reloadJS: path.join(assetDir, 'reload.js'),
-  settingsPath: '/_',
-  port: 8800,
-  execDir: execDir,
-  hosts: ['127.0.0.1', ipv4],
-  tmplDir: path.join(assetDir, 'settings/dist'),
-  projectConfig: projectConfig
-};
 
 
-app.engine('html', cons.ejs);
-app.engine('jade', cons.jade);
-app.engine('hbs', cons.handlebars);
-app.set('view engine', 'html');
+module.exports = function(port) {
+  global.app = express();
 
-//添加日志
-app.use(require('morgan')('dev'));
+  var server = require('http').Server(app);
 
-//添加settings
-app.all('/:pattern(*)',
-  require('./lib/server')(config)
-);
+  global.io = require('socket.io')(server, {
+    log: false,
+    origins: '*:*' //解决同源策略
+  });
+
+  var execDir = process.cwd();
+  var rootDir = path.join(__dirname);
+  var assetDir = path.join(rootDir, 'assets');
+  var projectConfig = {
+    folders: [execDir],
+    curDir: execDir
+  };
+  var config = {
+    rootDir: rootDir,
+    assetDir: assetDir,
+    reloadJS: path.join(assetDir, 'reload.js'),
+    settingsPath: '/_',
+    port: port || 8800,
+    execDir: execDir,
+    hosts: ['127.0.0.1', ipv4],
+    tmplDir: path.join(assetDir, 'settings/dist'),
+    projectConfig: projectConfig
+  };
 
 
-server.listen(config.port, function () {
-  $.log(('Mock Server is listening on port ' + config.port).green);
-});
+  app.engine('html', cons.ejs);
+  app.engine('jade', cons.jade);
+  app.engine('hbs', cons.handlebars);
+  app.set('view engine', 'html');
 
-process.on('uncaughtException', function (err) {
-  if (err.errno === 'EADDRINUSE' || err.errno === 'EACCES') {
-    $.log(('Mock Server:  Port ' + config.port + ' is already in use.').red);
-    process.exit(1);
-  } else
-    $.log(err);
-});
+  //添加日志
+  app.use(require('morgan')('dev'));
+
+  //添加settings
+  app.all('/:pattern(*)',
+    require('./lib/server')(config)
+  );
+
+
+  server.listen(config.port, function() {
+    var port = config.port;
+    var url = 'http://' + ipv4 + (port ? (':' + port) : port) + '/_/';
+
+    $.log(('Mock Server is listening on port ' + config.port).green);
+    $.log('setting Url is ==> '.red + url.red.bold);
+  });
+
+  process.on('uncaughtException', function(err) {
+    if (err.errno === 'EADDRINUSE' || err.errno === 'EACCES') {
+      $.log(('Mock Server:  Port ' + config.port + ' is already in use.').red);
+      process.exit(1);
+    } else
+      $.log(err);
+  });
+
+}
